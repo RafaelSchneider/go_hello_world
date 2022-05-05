5890926729c873a9f96a2927ac9d7a8dbeefb452
Rafael Schneider da Silva <rafael.schneider.s@TIMWE.COM>
2022-05-05 15:57:04 +0100

    adding mod

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-  "io"
-  "log"
-  "net/http"
+	"io"
+	"log"
+	"net/http"
 )
 
-func main(){
-  helloHandler := func(w http.ResponseWriter, req *http.Request) {
-  		io.WriteString(w, "Hello, world!\n")
-  	}
+func main() {
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
 
-  	http.HandleFunc("/hello", helloHandler)
-      log.Println("Listing for requests at http://localhost:8000/hello")
-  	log.Fatal(http.ListenAndServe(":8000", nil))
+	http.HandleFunc("/hello", helloHandler)
+	log.Default()
+	log.Println("Listing for requests at http://localhost:8000/hello")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
 }
